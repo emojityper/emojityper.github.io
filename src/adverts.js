85c882121c53a6d844b2f456c4cf6751158bb078
Sam Thorogood <sam.thorogood@gmail.com>
2019-02-01 14:27:30 +1100

    change adverts to rotate, use async

--- src/adverts.js
+++ src/adverts.js
@@ -1,30 +1,30 @@
+import {rAF} from './lib/promises.js';
 
 /**
  * @fileoverview Rotates through various advertisements in Emojityper.
  */
 
-const adverts = document.getElementById('adverts');
+const delay = 10 * 1000;
+const initialDelay = 2 * delay;
 
-function refresh() {
-  const active = adverts.querySelector('.active');
-  const next = (active && active.nextElementSibling || adverts.firstElementChild);
-  if (!next) {
-    console.warn('no adverts to choose from');
-    return;
+const rotate = document.getElementById('rotate');
+const all = Array.from(rotate.children);  // nb. these are in DOM for SEO
+
+async function run() {
+  await rAF(initialDelay);
+
+  const initial = all.shift();
+  initial.classList.remove('active');
+  all.sort(() => Math.random() - 0.5);
+
+  for (;;) {
+    all[0].classList.add('active');
+    await rAF(delay);
+
+    all[0].classList.remove('active');
+    all.push(all.shift());
   }
-
-  active && active.classList.remove('active');
-  next.classList.add('active');
-
-  enqueue();
 }
 
-let timeout;
-function enqueue() {
-  window.clearTimeout(timeout);
-  timeout = window.setTimeout(() => {
-    window.requestAnimationFrame(refresh);
-  }, 10 * 1000);
-}
+run().catch(console.warn);
 
-enqueue();
