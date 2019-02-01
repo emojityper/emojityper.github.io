79f67b05fffd174b454fa007b9951204fb184ab6
Sam Thorogood <sam.thorogood@gmail.com>
2019-02-01 14:27:40 +1100

    include more for support nomodule case

--- src/entrypoint/support.js
+++ src/entrypoint/support.js
@@ -6,3 +6,8 @@
 import '../support/check.js';
 import '../support/polyfill.js';
 import './main.js';
+
+// from ext.js, but included in bundle
+import '../adverts.js';
+import '../placeholders.js';
+import '../sw.js';
